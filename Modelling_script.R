ef9f29507906d162fc3d0ee2e573c021f0d3e669
awl75 <andrewlegan@gmail.com>
2020-03-02 15:33:40 -0500

    practice

--- Modelling_script.R
+++ Modelling_script.R
@@ -42,13 +42,47 @@ library(agridat)
 
 apples <- agridat::archbold.apple
 
+apples$spacing2 <- as.factor(apples$spacing)
+
+# visualize data with a box plot
+
+apples.p <- ggplot(apples, aes(spacing2, yield)) +
+    geom_boxplot(fill = "#CD3333", alpha = 0.8, colour = "#8B2323") +
+    theme.clean() +  
+    theme(axis.text.x = element_text(size = 12, angle = 0)) +
+    labs(x = "Spacing (m)", y = "Yield (kg)")
+
+# test lm
+
+apples.m <- lm(yield ~ spacing2, data = apples)
+summary(apples.m)
+
+# test lm again
+
+sheep <- agridat::ilri.sheep   # load the data
+
+library(dplyr)
+sheep <- filter(sheep, ewegen == "R")   # there are confounding variables in this dataset that we don't want to take into account. We'll only consider lambs that come from mothers belonging to the breed "R".
+
+head(sheep)  # overview of the data; we'll focus on weanwt (wean weight) and weanage
+
+sheep.m1 <- lm(weanwt ~ weanage, data = sheep)   # run the model
+summary(sheep.m1)                                # study the output
+
+
+
+
 # Using population trend data for the European Shag from the Living Planet Index
 # Import the shagLPI.csv file from the project's directory
+
+
 
 # Using a dataset on weevil damage to Scott's pine
 # Import the Weevil_damage.csv file from the project's directory
 
 # A simple linear model ----
+
+
 
 # Generalised linear models ----
 
